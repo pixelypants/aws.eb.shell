473bb7b83b4459fc18d2f6e6a751c20646b1450d
pixelypants <hudson@pixelypants.com>
2018-01-17 10:40:09 +1000

    Update bundle script to work within aws directory

--- aws/AwsBundle.ps1
+++ aws/AwsBundle.ps1
@@ -1,27 +1,42 @@
+function Remove-Folder
+{
+    Param(
+        [parameter(Mandatory = $true)]
+        [string]
+        $StagingDir,
+        [string]
+        $StagingDirectorName
+    )
+
+    if ((Test-Path -path $StagingDir))
+    {
+        Write-Host "Clearing $StagingDirectorName folder"
+        Remove-Item -Recurse -Force $StagingDir
+    }
+}
+
 function New-Bundle
 {
     $stagingDirName = "bundle"
     $rootDir = Convert-Path .
+    if ((Get-Item -Path ".\" -Verbose).Name -eq 'aws')
+    {
+        $rootDir = Convert-Path ..
+    }
     $stagingDir = $rootDir + "\$stagingDirName"
     $exclude = @('.vscode', 'aws', 'AwsBundle.ps1', 'AwsUpload.ps1', $stagingDirName)
-    
-    if ((Test-Path -path $stagingDir))
-    {
-        Write-Host "Clearing $stagingDirName folder"
-        Remove-Item -Recurse -Force $stagingDir
-    }
-    
+
+    Remove-Folder -StagingDir $stagingDir -StagingDirectorName $stagingDirName
+
     New-Item $stagingDir -type directory;
-    Copy-Item -Path .\aws\* -Destination $stagingDir -Recurse -Exclude $exclude
+    Copy-Item -Path "$rootDir\aws\*" -Destination $stagingDir -Recurse -Exclude $exclude
     
-    Copy-Item -Path .\* -Destination $stagingDir -Exclude $exclude
+    Copy-Item -Path "$rootDir\*" -Destination $stagingDir -Exclude $exclude 
     
-    $bundleZip = $rootDir + "\app.zip"
+    Compress-Archive -Path "$stagingDir\*" -DestinationPath "$rootDir\app.zip" -Force
     
-    Compress-Archive -Path "$stagingDir\*" -DestinationPath $bundleZip
-    
-    Remove-Item –path $stagingDir –recurse
+    Remove-Folder -StagingDir $stagingDir -StagingDirectorName $stagingDirName
 }
 
 Write-Host "Creating deployment bundle."
-Create-Bundle
+New-Bundle
